028abfd8a81f2f5b5a44c0ef5e1aeecb2053a242
zbt <2479879758@qq.com>
2022-04-28 18:31:22 +0800

    first commit

---
+++ src/views/layout/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <left-menu />
+    <div :class="{'main-app': true, 'menu-unfold': $store.state.collapsed}">
+      <slider-nav />
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import LeftMenu from './components/menu.vue';
+import SliderNav from './components/sliderNav.vue';
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+  components: {
+    LeftMenu,
+    SliderNav,
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="less">
+@import url("~@/assets/css/home.less");
+</style>
